98792aa2c83816bf38d05abb7eafbded4d57bc9b
Zain_Saleem <2004.zainsaleem@gmail.com>
2023-12-07 18:46:12 +0200

    Added description, images and links.

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ content2 = """Below you can find some of the apps
     I have built. Feel free to contact me."""
 st.info(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv",sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
